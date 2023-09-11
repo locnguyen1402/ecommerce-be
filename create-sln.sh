d8116ab83d9e911e53a5afff55df4e5f11c1dd33
Loc Nguyen <loc.nguyen@vklink.vn>
2023-09-11 17:55:05 +0700

    update create sln file

--- create-sln.sh
+++ create-sln.sh
@@ -3,5 +3,4 @@
 rm ecommerce-platform.sln
 
 dotnet new sln --name ecommerce-platform
-dotnet sln ecommerce-platform.sln add services/products/**/*.csproj
-dotnet sln ecommerce-platform.sln add services/shared/**/*.csproj
+dotnet sln ecommerce-platform.sln add services/**/**/*.csproj
