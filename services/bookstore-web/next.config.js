dad660b5e73cd0ce33efced8017a5434bbe86118
Loc Nguyen <loc.nguyen@vklink.vn>
2023-09-18 16:39:02 +0700

    update codes

--- services/bookstore-web/next.config.js
+++ services/bookstore-web/next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  reactStrictMode: true,
+  swcMinify: true,
+  modularizeImports: {
+    '@mui/icons-material': {
+      transform: '@mui/icons-material/{{member}}',
+    },
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'source.unsplash.com',
+        port: '',
+        pathname: '/random',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
