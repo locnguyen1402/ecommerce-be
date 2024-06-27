4d2e8c9dc0f98ea8a2fb4cb40aedaf2f24d3d77c
Loc Nguyen <loc.nguyen@vklink.vn>
2024-06-27 21:55:50 +0700

    add rule for product description

--- src/services/inventory/Inventory.Api/Api/Products/Requests/CreateProduct.cs
+++ src/services/inventory/Inventory.Api/Api/Products/Requests/CreateProduct.cs
@@ -25,6 +25,9 @@ public class CreateProductRequestValidator : AbstractValidator<CreateProductRequ
         RuleFor(x => x.Slug)
             .NotEmpty();
 
+        RuleFor(x => x.Description)
+            .MaximumLength(500);
+
         RuleFor(x => x.Attributes)
             .Must(x => x.Count == x.Distinct().Count())
             .WithMessage($"{PrefixErrorMessage} Attribute id must be unique");
