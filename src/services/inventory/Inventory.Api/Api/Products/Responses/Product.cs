d9f5ecffec6209d4fa72955ce3d55b2ff91b5c60
Loc Nguyen <loc.nguyen@vklink.vn>
2024-06-28 12:09:11 +0700

    apply order in product response

--- src/services/inventory/Inventory.Api/Api/Products/Responses/Product.cs
+++ src/services/inventory/Inventory.Api/Api/Products/Responses/Product.cs
@@ -1,4 +1,5 @@
 using System.Linq.Expressions;
+
 using ECommerce.Inventory.Api.Categories.Responses;
 using ECommerce.Inventory.Domain.AggregatesModel;
 
@@ -9,30 +10,51 @@ public record AdminProductDetailResponse(
     string Name,
     string Slug,
     string Description,
-    List<ProductVariantResponse> Variants,
     List<CategoryResponse> Categories
 )
 {
     private List<ProductAttributeResponse> ProductAttributes { get; } = [];
+    private List<ProductVariantResponse> ProductVariants { get; } = [];
     public AdminProductDetailResponse(
         Guid id,
         string name,
         string slug,
         string description,
+        List<CategoryResponse> categories,
         List<ProductAttributeResponse> productAttributes,
-        List<ProductVariantResponse> variants,
-        List<CategoryResponse> categories) : this(id, name, slug, description, variants, categories)
+        List<ProductVariantResponse> productVariants
+        ) : this(id, name, slug, description, categories)
     {
         ProductAttributes = productAttributes;
+        ProductVariants = productVariants;
     }
 
-    public decimal Price => Variants.Count > 0 ? Variants.Min(x => x.Price) : 0;
+    public decimal Price => ProductVariants.Count > 0 ? ProductVariants.Min(x => x.Price) : 0;
     public List<ProductAttributeWithValuesResponse> Attributes
         => ProductAttributes
             .Select(x => new ProductAttributeWithValuesResponse(
-                x.Id, x.Name,
-                Variants.SelectMany(v => v.Values.Where(a => a.AttributeId == x.Id).Select(a => a.Value)).ToHashSet())
-            )
+                x.Id
+                , x.Name
+                , ProductVariants
+                    .SelectMany(v => v.Values.Where(a => a.AttributeId == x.Id).Select(a => a.Value))
+                    .Order()
+                    .ToHashSet()
+            ))
+            .OrderBy(x => x.Name)
+            .ToList();
+
+    public List<ProductVariantResponse> Variants
+        => ProductVariants
+            .Select(variant => 
+            {
+                return new
+                {
+                    Variant = variant,
+                    SortKey = string.Join("|", variant.Values.OrderBy(x => ProductAttributes.Find(xx => xx.Id == x.AttributeId)!.Name).Select(x => x.Value))
+                };
+            })
+            .OrderBy(x => x.SortKey)
+            .Select(x => x.Variant)
             .ToList();
 };
 
@@ -50,6 +72,10 @@ public static class ProductProjection
             x.Name,
             x.Slug,
             x.Description,
+            x.Categories
+                .AsQueryable()
+                .Select(CategoryProjection.ToCategoryResponse())
+                .ToList(),
             x.ProductAttributes
                 .AsQueryable()
                 .Select(ProductAttributeProjection.ToProductAttributeResponse())
@@ -57,10 +83,6 @@ public static class ProductProjection
             x.ProductVariants
                 .AsQueryable()
                 .Select(ProductVariantProjection.ToProductVariantResponse())
-                .ToList(),
-            x.Categories
-                .AsQueryable()
-                .Select(CategoryProjection.ToCategoryResponse())
                 .ToList()
         );
 }
